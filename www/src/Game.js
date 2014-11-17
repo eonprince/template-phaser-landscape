cb7c3332837d57ca36aa2d015041ce7f197181cc
Cong Liu <cong.liu@intel.com>
2014-11-17 19:30:02 +0800

    Simplify the template

--- www/src/Game.js
+++ www/src/Game.js
@@ -1,51 +1,46 @@
+BasicGame = {
+
+};
 
 BasicGame.Game = function (game) {
-
-	//	When a State is added to Phaser it automatically has the following properties set on it, even if they already exist:
-
-    this.game;		//	a reference to the currently running game
-    this.add;		//	used to add sprites, text, groups, etc
-    this.camera;	//	a reference to the game camera
-    this.cache;		//	the game cache
-    this.input;		//	the global input manager (you can access this.input.keyboard, this.input.mouse, as well from it)
-    this.load;		//	for preloading assets
-    this.math;		//	lots of useful common math operations
-    this.sound;		//	the sound manager - add a sound, play one, set-up markers, etc
-    this.stage;		//	the game stage
-    this.time;		//	the clock
-    this.tweens;	//	the tween manager
-    this.world;		//	the game world
-    this.particles;	//	the particle manager
-    this.physics;	//	the physics manager
-    this.rnd;		//	the repeatable random number generator
-
-    //	You can use any of these from any function within this State.
-    //	But do consider them as being 'reserved words', i.e. don't create a property for your own game called "world" or you'll over-write the world reference.
-
 };
 
 BasicGame.Game.prototype = {
 
-	create: function () {
+    init: function () {
 
-		//	Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+        this.input.maxPointers = 1;
+        this.stage.disableVisibilityChange = true;
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.setMinMax(480, 260, 1024, 768);
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+        this.scale.forceOrientation(true, false);
+        this.scale.setResizeCallback(this.gameResized, this);
+        this.scale.setScreenSize(true);
+        this.scale.refresh();
 
-	},
+    },
 
-	update: function () {
+    preload: function () {
 
-		//	Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+        //  Here we load the assets required for our preloader (in this case a background and a loading bar)
+        this.load.image('logo', 'images/phaser.png');
+    },
 
-	},
+    create: function () {
 
-	quitGame: function (pointer) {
+        this.logo = this.add.sprite(this.world.centerX, this.world.centerY , 'logo');
+        this.logo.anchor.setTo(0.5, 0.5);
 
-		//	Here you should destroy anything you no longer need.
-		//	Stop music, delete sprites, purge caches, free resources, all that good stuff.
+    },
 
-		//	Then let's go back to the main menu.
-		this.state.start('MainMenu');
+    gameResized: function (width, height) {
 
-	}
+        //  This could be handy if you need to do any extra processing if the game resizes.
+        //  A resize could happen if for example swapping orientation on a device or resizing the browser window.
+        //  Note that this callback is only really useful if you use a ScaleMode of RESIZE and place it inside your main game state.
+
+    }
 
 };
